df8c2ca208a8d0e2d68994e6a489fab08d5a7591
reynolds.blair <reynoldsblair@gmail.com>
2019-05-17 11:38:05 -0400

    Added show ingredient.

---
+++ src/main/java/guru/springframework/services/IngredientService.java
@@ -0,0 +1,11 @@
+package guru.springframework.services;
+
+import guru.springframework.commands.IngredientCommand;
+
+/**
+ * Created by ed.blair on 5/17/2019.
+ */
+public interface IngredientService {
+
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+}
